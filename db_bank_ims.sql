674c0cdbc4accfe5e45857ab811d225ebaf4e35d
tugaspstiunud <72388016+GaluhGita20@users.noreply.github.com>
2022-03-17 22:41:21 +0800

    first commit

---
+++ db_bank_ims.sql
@@ -0,0 +1,43 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3308
+-- Generation Time: Mar 17, 2022 at 12:42 PM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 8.0.15
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_bank_ims`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_integrasi`
+--
+-- Error reading structure for table db_bank_ims.tb_integrasi: #1932 - Table 'db_bank_ims.tb_integrasi' doesn't exist in engine
+-- Error reading data for table db_bank_ims.tb_integrasi: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `db_bank_ims`.`tb_integrasi`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_transaksi`
+--
+-- Error reading structure for table db_bank_ims.tb_transaksi: #1932 - Table 'db_bank_ims.tb_transaksi' doesn't exist in engine
+-- Error reading data for table db_bank_ims.tb_transaksi: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `db_bank_ims`.`tb_transaksi`' at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
